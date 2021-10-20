a1c0d74d27d129c63ff4b9ae878df7490dcf1253
Hemil78 <zanattahemil@gmail.com>
2021-10-20 15:08:52 +0200

    header up

--- index.php
+++ index.php
@@ -14,9 +14,36 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+        <!-- fontawesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <title>Google Faq</title>
     </head>
     <body>
-        
+        <header>
+            <div class="header-up">
+                <div class="header-logo">
+                    <img src="img/googlelogo_clr_74x24px.svg" alt="">
+                    <h2>Privacy e Termini</h2>
+                </div>
+                <div class="header-profile">
+                    <div class="icons">
+                        <a href="#"><i class="fas fa-align-justify"></i></a>
+                    </div>
+                    <div class="phofo-profile">
+                        <span>H</span>
+                    </div>
+                </div>
+
+            </div>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
     </body>
 </html>
